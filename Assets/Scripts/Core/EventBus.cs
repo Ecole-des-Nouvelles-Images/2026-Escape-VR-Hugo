295d32ad77b9549914cbdf2449b9dc45f4d9f651
HIsambart <isambart.hugo0912@gmail.com>
2026-05-04 10:40:39 +0200

    Added events to EventBus

--- Assets/Scripts/Core/EventBus.cs
+++ Assets/Scripts/Core/EventBus.cs
@@ -1,4 +1,4 @@
-using Unity.Android.Gradle.Manifest;
+using System;
 
 namespace Core
 {
@@ -12,5 +12,33 @@ namespace Core
         
         // ===== CLOCK =====
         public static Action OnClockTimeChanged;
+        
+        // ===== PUZZLE CANDLE =====
+        public static Action OnFirstKeyUnlocked;
+        public static Action OnBriefcaseOpened;
+        public static Action OnCuckooClockRepaired;
+        public static Action OnCandleKeyUnlocked;
+        
+        // ===== PUZZLE LIGHT =====
+        public static Action OnStaticStatuetteEnlightened;
+        public static Action OnSecondStatuetteEnlightened;
+        public static Action OnMechanismUnlocked;
+        public static Action OnFirstElementActivated;
+        public static Action OnSecondElementActivated;
+        public static Action OnThirdElementActivated;
+        public static Action OnLightKeyUnlocked;
+        
+        // ===== PUZZLE FLOWER =====
+        public static Action OnFirstDrawerOpened;
+        public static Action OnSecondDrawerOpened;
+        public static Action OnThirdDrawerOpened;
+        public static Action OnAlarmRepaired;
+        public static Action OnFlowerKeyUnlocked;
+        
+        // ===== GENERAL PUZZLE =====
+        public static Action OnCandleKeyInserted;
+        public static Action OnLightKeyInserted;
+        public static Action OnFlowerKeyInserted;
+        public static Action OnLetterRecovered;
     }
 }
