8a814f688db9f7ded9c1b881d868091bd2670210
HIsambart <isambart.hugo0912@gmail.com>
2026-05-06 16:57:39 +0200

    Added BlendShape Temporal

---
+++ Assets/_Branches/Hugo/Scripts/PuzzleFlower/PetalsBouquetHandler.cs
@@ -0,0 +1,16 @@
+﻿using _Branches.Hugo.Scripts.Temporal;
+using UnityEngine;
+
+namespace _Branches.Hugo.Scripts.PuzzleFlower
+{
+    public class PetalsBouquetHandler : TemporalGameObject
+    {
+        [Header("===== REFERENCES =====")]
+        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+        
+        protected override void TimeBehavior()
+        {
+            _skinnedMeshRenderer.SetBlendShapeWeight(1, _state * 100);
+        }
+    }
+}
