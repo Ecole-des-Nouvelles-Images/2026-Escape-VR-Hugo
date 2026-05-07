5e2c9a0521810beda3dcbcb0e6b9a595d620902f
HIsambart <isambart.hugo0912@gmail.com>
2026-05-07 09:54:28 +0200

    Patched blend shapes

---
+++ Assets/_Branches/Hugo/Scripts/PuzzleFlower/TemporalSkinnedMeshHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using _Branches.Hugo.Scripts.Temporal;
+using UnityEngine;
+
+namespace _Branches.Hugo.Scripts.PuzzleFlower
+{
+    public class TemporalSkinnedMeshHandler : TemporalGameObject
+    {
+        [Header("===== REFERENCES =====")]
+        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+        [SerializeField] private List<int> _blendShapeIndexes = new();
+        
+        protected override void TimeBehavior()
+        {
+            foreach (var blendShapeIndex in _blendShapeIndexes)
+            {
+                _skinnedMeshRenderer.SetBlendShapeWeight(blendShapeIndex, _state * 100);
+            }
+        }
+    }
+}
