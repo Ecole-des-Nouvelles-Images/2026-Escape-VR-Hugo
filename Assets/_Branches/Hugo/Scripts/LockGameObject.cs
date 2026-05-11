ca272320737815b15ff96cda40f42ca472250570
HIsambart <isambart.hugo0912@gmail.com>
2026-05-11 16:32:52 +0200

    Added LockGameObject

---
+++ Assets/_Branches/Hugo/Scripts/LockGameObject.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+namespace _Branches.Hugo.Scripts
+{
+    public class LockGameObject : MonoBehaviour
+    {
+        private struct LockedObjectCache
+        {
+            public XRGrabInteractable Grab;
+            public Rigidbody Rb;
+        }
+
+        [Header("===== REFERENCES =====")]
+        [SerializeField] private List<GameObject> _lockGameObjects = new();
+
+        private List<LockedObjectCache> _cachedObjects = new();
+
+        private void Start()
+        {
+            foreach (var obj in _lockGameObjects)
+            {
+                if (obj == null) continue;
+
+                LockedObjectCache cache = new LockedObjectCache
+                {
+                    Grab = obj.GetComponent<XRGrabInteractable>(),
+                    Rb = obj.GetComponent<Rigidbody>()
+                };
+
+                if (cache.Grab) cache.Grab.enabled = false;
+                if (cache.Rb) cache.Rb.constraints = RigidbodyConstraints.FreezeAll;
+
+                _cachedObjects.Add(cache);
+            }
+        }
+
+        public void UnlockGameObjects()
+        {
+            foreach (var cached in _cachedObjects)
+            {
+                if (cached.Grab) cached.Grab.enabled = true;
+                if (cached.Rb) cached.Rb.constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
+}
