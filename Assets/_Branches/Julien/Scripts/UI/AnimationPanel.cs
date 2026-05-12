d471100c4f1cf7b2aa158ed8ab4ced60fce0d2a1
Julien Durand <Juliend1305@gmail.com>
2026-05-12 16:47:00 +0200

    Update Menu animation, disable Interaction menu when player is in game

--- Assets/_Branches/Julien/Scripts/UI/AnimationPanel.cs
+++ Assets/_Branches/Julien/Scripts/UI/AnimationPanel.cs
@@ -1,14 +1,50 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 
 public class AnimationPanel : MonoBehaviour
 {
+    public TypeAnimation AnimationType;
+    
+    [Header("Slide")]
+    RectTransform _rectTransform;
+    [SerializeField] Vector4 _startPosition;
+    [SerializeField] Vector4 _endPosition;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
     private void OnEnable()
     {
-        Vector3 scale = transform.localScale;
-        scale.y = 0;
-        transform.localScale = scale;
+        switch (AnimationType)
+        {
+            case TypeAnimation.Scale:
+                Vector3 scale = transform.localScale;
+                scale.y = 0;
+                transform.localScale = scale;
         
-        transform.DOScaleY(1, 0.5f);
+                transform.DOScaleY(1, 0.5f);
+                break;
+
+            case TypeAnimation.Slide:
+                _rectTransform.offsetMin = new Vector2(_startPosition.x, _rectTransform.offsetMin.y);
+                _rectTransform.offsetMax = new Vector2(-_startPosition.y, _rectTransform.offsetMax.y);
+
+                DOTween.To(() => _rectTransform.offsetMin, x => _rectTransform.offsetMin = x,
+                    new Vector2(_endPosition.x, _rectTransform.offsetMin.y), 0.5f).SetEase(Ease.OutQuint);
+                
+                DOTween.To(() => _rectTransform.offsetMax, x => _rectTransform.offsetMax = x, 
+                    new Vector2(-_endPosition.y, _rectTransform.offsetMax.y), 0.5f).SetEase(Ease.OutQuint);
+                break;
+        }
+        
     }
+
+    public enum TypeAnimation
+    {
+        Scale,
+        Slide
+    };
 }
