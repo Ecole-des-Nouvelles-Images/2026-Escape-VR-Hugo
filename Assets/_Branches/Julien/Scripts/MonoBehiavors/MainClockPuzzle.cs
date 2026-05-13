d95a7439ca042fff25d070f4e56e3633f0393220
Julien Durand <Juliend1305@gmail.com>
2026-05-13 14:48:34 +0200

    fzfe

--- Assets/_Branches/Julien/Scripts/MonoBehiavors/MainClockPuzzle.cs
+++ Assets/_Branches/Julien/Scripts/MonoBehiavors/MainClockPuzzle.cs
@@ -4,16 +4,5 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MainClockPuzzle : MonoBehaviour
 {
-    [SerializeField] private GameObject _rightKey;
-    [SerializeField] private UnityEvent _onKeyInsert;
-    
-    public void InsertKey(SelectEnterEventArgs arg)
-    {
-        GameObject obj = arg.interactableObject.transform.gameObject;
-        
-        if (obj == _rightKey)
-        {
-            
-        }
-    }
+   
 }
