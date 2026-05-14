5c8a0ca64f84fb0ad56be9654d5a663d8a8072f1
Julien <juliend1305@gmail.com>
2026-05-14 15:49:03 +0200

    Rework transition scene

--- Assets/_Branches/Julien/Scripts/Managers/SceneLoaderManager.cs
+++ Assets/_Branches/Julien/Scripts/Managers/SceneLoaderManager.cs
@@ -7,8 +7,8 @@ using UnityEngine.SceneManagement;
 public class SceneLoaderManager : MonoBehaviourSingleton<SceneLoaderManager>
 {
     // _ApertureSize
-    [Range(0,1)] public float _valueBlackEffect = 1;
-    [Range(0,1)] public float _valueSmoothEffect = 0.8f;
+    [Range(0, 1)] public float _valueBlackEffect;
+    [Range(0,1)] public float _valueSmoothEffect = 1f;
     [SerializeField] public Material _blackScreenGm;
     [SerializeField] private bool _sceneLoaded = false;
     
@@ -28,29 +28,23 @@ public class SceneLoaderManager : MonoBehaviourSingleton<SceneLoaderManager>
 
     private void Update()
     {
-        _blackScreenGm.SetFloat("_ApertureSize", _valueBlackEffect);
+        //_blackScreenGm.SetFloat("_ApertureSize", _valueBlackEffect);
         _blackScreenGm.SetFloat("_FeatheringEffect", _valueSmoothEffect);
     }
 
     [ContextMenu("Enable")]
     public void EnableBlackScreen()
     {
-        DOTween.To(() => _valueBlackEffect, x => _valueBlackEffect = x, 0, 1f).OnComplete(() =>
+        DOTween.To(() => _valueSmoothEffect, x => _valueSmoothEffect = x, 0, 1.5f).OnComplete(() =>
         {
-            DOTween.To(() => _valueSmoothEffect, x => _valueSmoothEffect = x, 0, 1).OnComplete(() =>
-            {
-                StartCoroutine("TimeToLoad");
-            });
+            StartCoroutine("TimeToLoad");
         });
     }
     
     [ContextMenu("Disable")]
     private void DisableBlackScreen()
     {
-        DOTween.To(() => _valueBlackEffect, x => _valueBlackEffect = x, 1, 1f).OnComplete(() =>
-        {
-            DOTween.To(() => _valueSmoothEffect, x => _valueSmoothEffect = x, 1, 1f);
-        });
+        DOTween.To(() => _valueSmoothEffect, x => _valueSmoothEffect = x, 1, 1.5f);
     }
 
     private IEnumerator TimeToLoad()
