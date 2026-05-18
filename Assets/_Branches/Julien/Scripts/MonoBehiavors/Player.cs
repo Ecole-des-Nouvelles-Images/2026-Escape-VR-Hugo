3260207e314be4a5edcb0bfeac9165053e8ee511
Julien <juliend1305@gmail.com>
2026-05-18 10:57:12 +0200

    Minor change

--- Assets/_Branches/Julien/Scripts/MonoBehiavors/Player.cs
+++ Assets/_Branches/Julien/Scripts/MonoBehiavors/Player.cs
@@ -5,6 +5,9 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject _interactorRayLeft;
     [SerializeField] private GameObject _interactorRayRight;
     
+    [Header("PauseMenu")]
+    [SerializeField] private GameObject _pauseMenu;
+    
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -14,5 +17,10 @@ public class Player : MonoBehaviour
     {
         _interactorRayLeft.SetActive(false);
         _interactorRayRight.SetActive(false);
+    }
+
+    public void OpenPauseMenu()
+    {
+        
     }
 }
