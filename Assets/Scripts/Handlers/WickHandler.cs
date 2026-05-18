9ad4bb93747c1a96d9f3e884484eb0d80268a92d
Julien <juliend1305@gmail.com>
2026-05-18 16:20:55 +0200

    Ajust name space, move folder in int

---
+++ Assets/Scripts/Handlers/WickHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Handlers
+{
+    public class WickHandler : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _event;
+    
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Flame"))
+            {
+                _event?.Invoke();
+            }
+        }
+    }
+}
