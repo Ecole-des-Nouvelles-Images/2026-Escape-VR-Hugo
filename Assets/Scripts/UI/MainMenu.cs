61cd2e33e55914eb74457a04c72aba6f20e385c3
Julien Durand <Juliend1305@gmail.com>
2026-05-19 14:06:20 +0200

    Rework menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +12,20 @@ namespace UI
         [SerializeField] private GameObject _optionPanel;
         [SerializeField] private GameObject _creditsPanel;
         [SerializeField] private GameObject _exitPanel;
-    
+
+        [Header("POsitions Clock")] 
+        [SerializeField] private Vector2 _positionWhenOption;
+        [SerializeField] private Vector2 _positionWhenCredits;
+        [SerializeField] private Vector2 _positionWhenExit;
+
+        [SerializeField] private GameObject _aiguille;
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
         public void Play()
         {
             SceneManager.LoadScene(_sceneNameToLoad);
@@ -22,6 +37,7 @@ namespace UI
             _optionPanel.SetActive(true);
             _creditsPanel.SetActive(false);
             _exitPanel.SetActive(false);
+            MoveClockPanelToPosition(_positionWhenOption);
         }
 
         public void Credits()
@@ -29,6 +45,7 @@ namespace UI
             _creditsPanel.SetActive(true);
             _optionPanel.SetActive(false);
             _exitPanel.SetActive(false);
+            MoveClockPanelToPosition(_positionWhenCredits);
         }
 
         public void Quit()
@@ -36,11 +53,30 @@ namespace UI
             _exitPanel.SetActive(true);
             _optionPanel.SetActive(false);
             _creditsPanel.SetActive(false);
+            MoveClockPanelToPosition(_positionWhenExit);
         }
 
         public void QuitGame()
         {
             Application.Quit();
+        }
+        
+        [ContextMenu("Test")]
+        public void MoveClockPanelToPosition(Vector2 position)
+        {
+            DOTween.To(() => _rectTransform.offsetMax, x => _rectTransform.offsetMax = x,
+                new Vector2(0, position.x), 0.5f
+            );
+
+            DOTween.To(() => _rectTransform.offsetMin, x => _rectTransform.offsetMin = x, 
+                new Vector2(0, position.y), 0.5f
+            );
+        }
+
+        public void OnButtonSelected(Vector3 position)
+        {
+            Debug.Log(position);
+            _aiguille.transform.DOLookAt(position, 0.3f);
         }
     }
 }
