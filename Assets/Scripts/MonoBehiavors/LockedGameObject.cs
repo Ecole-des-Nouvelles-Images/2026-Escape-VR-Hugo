1bd593039a523324968609e7e49a7f68f92cfd37
HIsambart <isambart.hugo0912@gmail.com>
2026-05-28 20:12:54 +0200

    Added locked system

--- Assets/Scripts/MonoBehiavors/LockedGameObject.cs
+++ Assets/Scripts/MonoBehiavors/LockedGameObject.cs
@@ -1,6 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 namespace MonoBehiavors
@@ -12,39 +12,185 @@ namespace MonoBehiavors
             public XRGrabInteractable Grab;
             public Rigidbody Rb;
         }
+        
+        [Header("===== SETTINGS =====")]
+        [SerializeField] private bool _isLocked = true;
 
-        [FormerlySerializedAs("_lockGameObjects")]
-        [Header("===== REFERENCES =====")]
-        [SerializeField] private List<GameObject> _lockedGameObjects = new();
+        [Header("===== REFERENCES GAMEOBJECTS =====")]
+        [SerializeField] private GameObject _context;
+        [SerializeField] private List<GameObject> _insideGameObjects = new();
+        
+        [Header("==== TRIGGER =====")]
+        [SerializeField] private TriggerCollider _insideTrigger;
+        
+        [Header("===== RAYCAST =====")]
+        [SerializeField] private Transform _raycastOrigin;
+        [SerializeField] private float _raycastLength = 5f;
+        [SerializeField] private LayerMask _raycastMask;
 
-        private List<LockedObjectCache> _cachedObjects = new();
+        [Header("===== DEBUG =====")]
+        [SerializeField] private bool _isOpen;
 
-        private void Start()
+        private LockedObjectCache _cachedContext;
+        private GameObject _lastHitObject;
+
+        private IEnumerator Start()
         {
-            foreach (var obj in _lockedGameObjects)
-            {
-                if (obj == null) continue;
+            if (_raycastOrigin == null) _raycastOrigin = transform;
 
-                LockedObjectCache cache = new LockedObjectCache
+            if (_isLocked)
+            {
+                _cachedContext = FindComponentsEverywhere(_context);
+            
+                if (_cachedContext.Grab) _cachedContext.Grab.enabled = false;
+                if (_cachedContext.Rb) _cachedContext.Rb.constraints = RigidbodyConstraints.FreezeAll;
+            }
+            
+            yield return new WaitForEndOfFrame();
+            
+            _insideGameObjects = _insideTrigger.GetGameObjects();
+            if (_insideGameObjects.Count > 0)
+            {
+                foreach (var obj in _insideGameObjects)
                 {
-                    Grab = obj.GetComponent<XRGrabInteractable>(),
-                    Rb = obj.GetComponent<Rigidbody>()
-                };
-
-                if (cache.Grab) cache.Grab.enabled = false;
-                if (cache.Rb) cache.Rb.constraints = RigidbodyConstraints.FreezeAll;
-
-                _cachedObjects.Add(cache);
+                    if (obj == null) continue;
+                    ApplyLockState(obj, true);
+                }
             }
         }
 
-        public void UnlockGameObjects()
+        private void Update()
         {
-            foreach (var cached in _cachedObjects)
+            ExecuteRaycastCheck();
+        }
+
+        #region ===== PUBLIC =====
+
+        public void UnlockContext()
+        {
+            if (_cachedContext.Grab) _cachedContext.Grab.enabled = true;
+            if (_cachedContext.Rb) _cachedContext.Rb.constraints = RigidbodyConstraints.None;
+        }
+
+        #endregion
+        
+        #region ===== PRIVATE =====
+        
+        private void ExecuteRaycastCheck()
+        {
+            Vector3 origin = _raycastOrigin.position;
+            Vector3 direction = _raycastOrigin.forward;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, _raycastLength, _raycastMask))
             {
-                if (cached.Grab) cached.Grab.enabled = true;
-                if (cached.Rb) cached.Rb.constraints = RigidbodyConstraints.None;
+                if (!_lastHitObject)
+                {
+                    GameObject currentHitObject = hit.collider.gameObject;
+                    
+                    LockGameObjects();
+                    _lastHitObject = currentHitObject;
+
+                    _isOpen = false;
+                }
+            }
+            else
+            {
+                if (_lastHitObject)
+                {
+                    UnlockGameObjects();
+                    _lastHitObject = null;
+                    
+                    _isOpen = true;
+                }
             }
         }
+        
+        private void LockGameObjects()
+        {
+            _insideGameObjects = _insideTrigger.GetGameObjects();
+            if (_insideGameObjects.Count > 0)
+            {
+                foreach (var obj in _insideGameObjects)
+                {
+                    if (!obj) continue;
+                    ApplyLockState(obj, true);
+                }
+            }
+        }
+        
+        private void UnlockGameObjects()
+        {
+            _insideGameObjects = _insideTrigger.GetGameObjects();
+            if (_insideGameObjects.Count > 0)
+            {
+                foreach (var obj in _insideGameObjects)
+                {
+                    if (!obj) continue;
+                    ApplyLockState(obj, false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Applique l'état de verrouillage de manière sécurisée en cherchant partout dans la hiérarchie
+        /// </summary>
+        private void ApplyLockState(GameObject target, bool shouldLock)
+        {
+            LockedObjectCache cache = FindComponentsEverywhere(target);
+
+            if (cache.Grab) 
+            {
+                cache.Grab.enabled = !shouldLock;
+            }
+
+            // if (cache.Rb) 
+            // {
+            //     cache.Rb.constraints = shouldLock ? RigidbodyConstraints.FreezeAll : RigidbodyConstraints.None;
+            // }
+        }
+        
+        /// <summary>
+        /// Méthode outil robuste qui va chercher les composants n'importe où dans la hiérarchie de l'objet
+        /// </summary>
+        private LockedObjectCache FindComponentsEverywhere(GameObject target)
+        {
+            LockedObjectCache cache = new LockedObjectCache();
+            if (target == null) return cache;
+
+            cache.Grab = target.GetComponent<XRGrabInteractable>();
+            if (cache.Grab == null) cache.Grab = target.GetComponentInParent<XRGrabInteractable>();
+            if (cache.Grab == null) cache.Grab = target.GetComponentInChildren<XRGrabInteractable>();
+
+            // cache.Rb = target.GetComponent<Rigidbody>();
+            // if (cache.Rb == null) cache.Rb = target.GetComponentInParent<Rigidbody>();
+            // if (cache.Rb == null) cache.Rb = target.GetComponentInChildren<Rigidbody>();
+
+            return cache;
+        }
+        
+        #endregion
+        
+        #region ===== UNITY GIZMOS =====
+
+        private void OnDrawGizmos()
+        {
+            Transform originTransform = _raycastOrigin != null ? _raycastOrigin : transform;
+            Vector3 origin = originTransform.position;
+            Vector3 direction = originTransform.forward;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, _raycastLength, _raycastMask))
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(origin, hit.point);
+                Gizmos.DrawWireSphere(hit.point, 0.02f);
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(origin, origin + direction * _raycastLength);
+            }
+        }
+
+        #endregion
     }
 }
